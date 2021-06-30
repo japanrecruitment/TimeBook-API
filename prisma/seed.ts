c1647dba0f70e86860f5654a25297d371a09848e
vnshgrg <ghale.avinash@gmail.com>
2021-06-30 10:27:14 +0900

    refactored seed.ts & added prisma information to readme

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,40 @@
-import { users } from "./seeds/users";
 import { PrismaClient } from "@prisma/client";
-import { encodePassword, environment, Log } from "../src/utils";
+import { environment, Log } from "../src/utils";
+
+import { users, userProcessor } from "./seeds/users";
+
 const prisma = new PrismaClient();
 
 const main = async () => {
+    // run only in the dev environment
     if (environment.isDev()) {
         Log("Running on dev environment");
-        Log("Clearing old records...");
-        await prisma.user.deleteMany();
-        Log("Finished clearing data");
-        Log("Seeding new records...");
-        const processedUsers = users.map((user) => {
-            user.password = encodePassword(user.password);
-            return user;
-        });
-        await prisma.user.createMany({ data: processedUsers });
+        // Call seedTable function for each schema
+        await seedTable("user", users, userProcessor);
     } else {
         Log("Looks like you're trying to seed on production environment...");
         throw new Error("Can't seed on production environment!");
+    }
+};
+
+type DataProcessor<T> = (T) => T;
+
+const seedTable = async <T>(schema: string, data: T[], dataProcessor: DataProcessor<T> | null): Promise<void> => {
+    try {
+        if (dataProcessor) {
+            Log(`${schema}: Processing data...`);
+            data = data.map(dataProcessor);
+        }
+        // clear out the existing data
+        Log(`${schema}: Clearing old records...`);
+        await prisma[schema].deleteMany();
+        Log(`${schema}: Finish clearing data`);
+
+        Log(`${schema}: Seeding new data...`);
+        await prisma[schema].createMany({ data });
+        Log(`${schema}: Seeding complete`);
+    } catch (error) {
+        throw new Error(`Error while seeding ${schema}: ${error.message}`);
     }
 };
 
