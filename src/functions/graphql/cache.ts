4c299106bf7365a8f27ee0f66a168bc0d97dab54
vnshgrg <ghale.avinash@gmail.com>
2021-07-28 19:18:39 +0900

    fixed: sls webpack dependency build fail

--- src/functions/graphql/cache.ts
+++ src/functions/graphql/cache.ts
@@ -2,8 +2,10 @@ import { Log, environment } from "@utils/index";
 import { RedisCache } from "apollo-server-cache-redis";
 
 export default new RedisCache({
-    host: environment.IS_LOCAL ? "localhost" : environment.REDIS_HOST,
-    port: environment.IS_LOCAL ? "6379" : environment.REDIS_PORT,
+    host: environment.REDIS_HOST,
+    port: environment.REDIS_PORT,
+    // host: "localhost",
+    // port: "6379",
     connectTimeout: 5000,
     reconnectOnError: function (err) {
         Log("Reconnect on error", err);
