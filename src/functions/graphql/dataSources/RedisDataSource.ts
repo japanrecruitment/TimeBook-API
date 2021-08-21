8bf6aec33bd9777412b4fd89960db7e5ee231bd4
susantlalshrestha <susantlalshrestha@gmail.com>
2021-08-21 18:53:19 +0545

    removed: converting cache key to buffer

--- src/functions/graphql/dataSources/RedisDataSource.ts
+++ src/functions/graphql/dataSources/RedisDataSource.ts
@@ -17,35 +17,6 @@ export default class CacheDataSource<TContext = any> extends DataSource {
         this.cache = cache instanceof RedisCache && cache;
     }
 
-    async fetchFromCache<TData = any>(key: number | string): Promise<TData | null | undefined> {
-        try {
-            Log("[STARTED]: Fetching data from cache.");
-            const cacheKey = Buffer.from(`${this.cachePrefix}-${key}`).toString("base64");
-            const cacheDoc = await this.cache?.get(cacheKey);
-            Log(`[${cacheDoc ? "COMPLETED" : "FAILED"}]: Fetching data from cache.`);
-            if (cacheDoc) return JSON.parse(cacheDoc);
-        } catch (error) {
-            Log("[FAILED]: Fetching data from cache.");
-            Log(error);
-        }
-    }
-
-    async storeInCache<TData = any>(key: number | string, doc: TData, ttl: number) {
-        Log("[STARTED]: Storing data in cache.");
-        const cacheKey = Buffer.from(`${this.cachePrefix}-${key}`).toString("base64");
-        if (Number.isInteger(ttl)) {
-            this.cache
-                ?.set(cacheKey, JSON.stringify(doc), { ttl })
-                .then(() => {
-                    Log("[COMPLETED]: Storing data in cache.");
-                })
-                .catch((error) => {
-                    Log("[FAILED]: Storing data in cache.");
-                    Log(error);
-                });
-        }
-    }
-
     async delete(key: number | string) {
         try {
             Log("[STARTED]: Deleting data from cache.");
@@ -53,6 +24,37 @@ export default class CacheDataSource<TContext = any> extends DataSource {
             Log("[COMPLETED]: Deleting data from cache.");
         } catch (error) {
             Log("[FAILED]: Deleting data from cache.");
+            Log(error);
+        }
+    }
+
+    async deleteMany(pattern: string) {
+        Log([`[STARTED]: Deleting from cache with key matching ${pattern}`]);
+        const stream = this.client.scanStream({ match: `${this.cachePrefix}-${pattern}` });
+        stream.on("data", (keys) => {
+            if (!keys?.length) return;
+            Log([`[DELETING]: Deleting ${keys.length} records with key matching ${pattern}`]);
+            const pipeline = this.client.pipeline();
+            keys.forEach((key) => pipeline.del(key));
+            pipeline.exec();
+        });
+        stream.on("end", () => {
+            Log([`[COMPLETED]: Deleting from cache with key matching ${pattern}`]);
+        });
+        stream.on("error", () => {
+            Log([`[FAILED]: Deleting from cache with key matching ${pattern}`]);
+        });
+    }
+
+    async fetchFromCache<TData = any>(key: number | string): Promise<TData | null | undefined> {
+        try {
+            Log("[STARTED]: Fetching data from cache.");
+            const cacheKey = `${this.cachePrefix}-${key}`;
+            const cacheDoc = await this.cache?.get(cacheKey);
+            Log(`[${cacheDoc ? "COMPLETED" : "FAILED"}]: Fetching data from cache.`);
+            if (cacheDoc) return JSON.parse(cacheDoc);
+        } catch (error) {
+            Log("[FAILED]: Fetching data from cache.");
             Log(error);
         }
     }
@@ -69,21 +71,19 @@ export default class CacheDataSource<TContext = any> extends DataSource {
         }
     }
 
-    async deleteMany(pattern: string) {
-        Log([`[STARTED]: Deleting from cache with key matching ${pattern}`]);
-        const stream = this.client.scanStream({ match: pattern });
-        stream.on("data", (keys) => {
-            if (!keys?.length) return;
-            Log([`[DELETING]: Deleting ${keys.length} records with key matching ${pattern}`]);
-            const pipeline = this.client.pipeline();
-            keys.forEach((key) => pipeline.del(key));
-            pipeline.exec();
-        });
-        stream.on("end", () => {
-            Log([`[COMPLETED]: Deleting from cache with key matching ${pattern}`]);
-        });
-        stream.on("error", () => {
-            Log([`[FAILED]: Deleting from cache with key matching ${pattern}`]);
-        });
+    async storeInCache<TData = any>(key: number | string, doc: TData, ttl: number) {
+        Log("[STARTED]: Storing data in cache.");
+        const cacheKey = `${this.cachePrefix}-${key}`;
+        if (Number.isInteger(ttl)) {
+            this.cache
+                ?.set(cacheKey, JSON.stringify(doc), { ttl })
+                .then(() => {
+                    Log("[COMPLETED]: Storing data in cache.");
+                })
+                .catch((error) => {
+                    Log("[FAILED]: Storing data in cache.");
+                    Log(error);
+                });
+        }
     }
 }
