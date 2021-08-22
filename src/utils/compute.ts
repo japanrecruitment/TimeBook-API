2895c6e75ffd698e9dc6aebf3238280a0f91ddf3
susantlalshrestha <susantlalshrestha@gmail.com>
2021-08-22 08:55:57 +0545

    added: function to convert to or from seconds to other time units

--- src/utils/compute.ts
+++ src/utils/compute.ts
@@ -4,3 +4,56 @@ export const randomNumberOfNDigits = (n: number) => {
 
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+type TimeUnits = "ns" | "μs" | "ms" | "s" | "min" | "hrs" | "d" | "wk" | "mth" | "yr";
+
+export const seconds = {
+    from: (n: number, unit: TimeUnits) => {
+        switch (unit) {
+            case "ns":
+                return n / 1e9;
+            case "μs":
+                return n / 1e6;
+            case "ms":
+                return n / 1000;
+            case "min":
+                return n * 60;
+            case "hrs":
+                return n * 3600;
+            case "d":
+                return n * 86400;
+            case "wk":
+                return n * 604800;
+            case "mth":
+                return n * 2.628e6;
+            case "yr":
+                return n * 3.154e7;
+            default:
+                return n;
+        }
+    },
+    to: (n: number, unit: TimeUnits) => {
+        switch (unit) {
+            case "ns":
+                return n * 1e9;
+            case "μs":
+                return n * 1e6;
+            case "ms":
+                return n * 1000;
+            case "min":
+                return n / 60;
+            case "hrs":
+                return n / 3600;
+            case "d":
+                return n / 86400;
+            case "wk":
+                return n / 604800;
+            case "mth":
+                return n / 2.628e6;
+            case "yr":
+                return n / 3.154e7;
+            default:
+                return n;
+        }
+    },
+};
