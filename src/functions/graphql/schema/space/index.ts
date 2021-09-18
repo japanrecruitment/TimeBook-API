3df1bc0d3118b89eb1ecb817c2913d31393ac6cd
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-18 20:07:46 +0545

    added: nearest station typedefs and resolvers in gql schema

--- src/functions/graphql/schema/space/index.ts
+++ src/functions/graphql/schema/space/index.ts
@@ -2,7 +2,7 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { addSpaceResolvers, addSpaceTypeDefs } from "./addSpace";
 import { allSpacesTypeDefs, allSpacesResolvers } from "./allSpaces";
 import { mySpacesResolvers, mySpacesTypeDefs } from "./mySpaces";
-import { nearestStationTypeDefs } from "./nearestStation";
+import { nearestStationResolvers, nearestStationTypeDefs } from "./nearest-stations";
 import { spaceByIdResolvers, spaceByIdTypeDefs } from "./spaceById";
 import { spacePricePlanTypeDefs } from "./spacePricePlan";
 import { updateMySpaceResolvers, updateMySpaceTypeDefs } from "./updateMySpace";
@@ -21,6 +21,7 @@ export const spaceResolvers = mergeResolvers([
     addSpaceResolvers,
     allSpacesResolvers,
     mySpacesResolvers,
+    nearestStationResolvers,
     spaceByIdResolvers,
     updateMySpaceResolvers,
 ]);
--- src/functions/graphql/schema/space/nearest-stations/index.ts
+++ src/functions/graphql/schema/space/nearest-stations/index.ts
@@ -1,1 +1,23 @@
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { nearestStationObjectTypeDefs } from "./NearestStationObject";
+import { addNearestStationResolvers, addNearestStationTypeDefs } from "./addNearestStation";
+import { removeNearestStationResolvers, removeNearestStationTypeDefs } from "./removeNearestStation";
+import { updateNearestStationResolvers, updateNearestStationTypeDefs } from "./updateNearestStaion";
+
+export const nearestStationTypeDefs = mergeTypeDefs([
+    nearestStationObjectTypeDefs,
+    addNearestStationTypeDefs,
+    removeNearestStationTypeDefs,
+    updateNearestStationTypeDefs,
+]);
+
+export const nearestStationResolvers = mergeResolvers([
+    addNearestStationResolvers,
+    removeNearestStationResolvers,
+    updateNearestStationResolvers,
+]);
+
 export * from "./NearestStationObject";
+export * from "./addNearestStation";
+export * from "./removeNearestStation";
+export * from "./updateNearestStaion";
