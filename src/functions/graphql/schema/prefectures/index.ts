0e1d875937054bd6593787c2878798aec58af777
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-23 20:56:36 +0545

    added: query to fetch prefecture by id

--- src/functions/graphql/schema/prefectures/index.ts
+++ src/functions/graphql/schema/prefectures/index.ts
@@ -1,5 +1,6 @@
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { allPrefecturesResolvers, allPrefecturesTypeDefs } from "./allPrefectures";
+import { prefectureByIdResolvers, prefectureByIdTypeDefs } from "./prefectureById";
 import { prefecturesResolvers, prefecturesTypeDefs } from "./prefectures";
 import { updatePrefectureResolvers, updatePrefectureTypeDefs } from "./updatePrefecture";
 
@@ -7,10 +8,12 @@ export const prefectureTypeDefs = mergeTypeDefs([
     prefecturesTypeDefs,
     allPrefecturesTypeDefs,
     updatePrefectureTypeDefs,
+    prefectureByIdTypeDefs,
 ]);
 
 export const prefectureResolvers = mergeResolvers([
     prefecturesResolvers,
     allPrefecturesResolvers,
     updatePrefectureResolvers,
+    prefectureByIdResolvers,
 ]);
