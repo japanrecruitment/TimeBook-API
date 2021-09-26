d4e7f32c16639b05502d57090fa8e61e065a5184
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-26 21:50:57 +0545

    fixed: minor typo error

--- src/functions/graphql/schema/space/space-types/addSpaceType.ts
+++ src/functions/graphql/schema/space/space-types/addSpaceType.ts
@@ -2,8 +2,6 @@ import { IFieldResolver } from "@graphql-tools/utils";
 import { gql } from "apollo-server-core";
 import { GqlError } from "src/functions/graphql/error";
 import { Context } from "../../../context";
-import { toSpaceTypeSelect } from ".";
-import { mapSelections } from "graphql-map-selections";
 import { Result } from "../../core/result";
 import { ImageUploadResult } from "../../media";
 import { S3Lib } from "@libs/S3";
@@ -11,7 +9,7 @@ import { S3Lib } from "@libs/S3";
 type AddSpaceTypeInput = {
     title: string;
     description: string;
-    photoMime: string;
+    coverPhotoMime: string;
 };
 
 type AddSpaceTypeArgs = { input: AddSpaceTypeInput };
@@ -23,23 +21,23 @@ type AddSpaceTypeResult = Result & {
 type AddSpaceType = IFieldResolver<any, Context, AddSpaceTypeArgs, Promise<AddSpaceTypeResult>>;
 
 const addSpaceType: AddSpaceType = async (_, { input }, { dataSources, store }, info) => {
-    let { title, description, photoMime } = input;
+    let { title, description, coverPhotoMime } = input;
     title = title.trim();
     description = description.trim();
-    photoMime = photoMime.trim() || "image/jpeg";
+    coverPhotoMime = coverPhotoMime.trim() || "image/jpeg";
 
     if (!title) throw new GqlError({ code: "BAD_USER_INPUT", message: "Invalid title" });
 
     if (!description) throw new GqlError({ code: "BAD_USER_INPUT", message: "Invalid description" });
 
-    if (!photoMime) throw new GqlError({ code: "BAD_USER_INPUT", message: "Invalid mime" });
+    if (!coverPhotoMime) throw new GqlError({ code: "BAD_USER_INPUT", message: "Invalid mime" });
 
     const spaceType = await store.spaceType.findFirst({ where: { title } });
 
     if (spaceType) throw new GqlError({ code: "BAD_REQUEST", message: "The title for space type is already in use" });
 
     const newSpaceType = await store.spaceType.create({
-        data: { title, description, photo: { create: { mime: photoMime, type: "Cover" } } },
+        data: { title, description, photo: { create: { mime: coverPhotoMime, type: "Cover" } } },
         include: { photo: { select: { id: true, mime: true } } },
     });
 
@@ -48,12 +46,12 @@ const addSpaceType: AddSpaceType = async (_, { input }, { dataSources, store }, 
     // get signedURL
     const key = `${newSpaceType.photo.id}.${newSpaceType.photo.mime.split("/")[1]}`;
     const S3 = new S3Lib("upload");
-    const signedURL = S3.getUploadUrl(key, photoMime, 60 * 10);
+    const signedURL = S3.getUploadUrl(key, coverPhotoMime, 60 * 10);
 
     return {
         message: `Successfully added ${title} space type`,
         action: "upload-cover-photo",
-        upload: { type: "Cover", mime: photoMime, url: signedURL, key },
+        upload: { type: "Cover", mime: coverPhotoMime, url: signedURL, key },
     };
 };
 
