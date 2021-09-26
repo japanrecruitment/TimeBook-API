27a9ec2a6438ee3fc6cf3b9f4bbe989243925e0d
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-26 20:51:07 +0545

    added: mutations for deleting cache data

--- src/functions/graphql/schema/cache/index.ts
+++ src/functions/graphql/schema/cache/index.ts
@@ -2,11 +2,18 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { environment } from "@utils/environment";
 import { allCacheKeysTypeDefs, allCacheKeysResolvers } from "./allCacheKeys";
 import { getCacheDataResolvers, getCacheDataTypeDefs } from "./getCacheData";
+import { deleteCacheDataTypeDefs, deleteCacheDataResolvers } from "./deleteCacheData";
+import { deleteManyCacheDataTypeDefs, deleteManyCacheDataResolvers } from "./deleteManyCacheData";
 
 export const cacheTypeDefs = environment.isDev()
-    ? mergeTypeDefs([allCacheKeysTypeDefs, getCacheDataTypeDefs])
+    ? mergeTypeDefs([allCacheKeysTypeDefs, getCacheDataTypeDefs, deleteCacheDataTypeDefs, deleteManyCacheDataTypeDefs])
     : undefined;
 
 export const cacheResolvers = environment.isDev()
-    ? mergeResolvers([allCacheKeysResolvers, getCacheDataResolvers])
+    ? mergeResolvers([
+          allCacheKeysResolvers,
+          getCacheDataResolvers,
+          deleteCacheDataResolvers,
+          deleteManyCacheDataResolvers,
+      ])
     : undefined;
