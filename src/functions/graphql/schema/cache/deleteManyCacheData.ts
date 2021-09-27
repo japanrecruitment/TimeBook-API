46da754d5c541d9eeb8e11887854e92612b0044e
susantlalshrestha <susantlalshrestha@gmail.com>
2021-09-27 19:47:45 +0545

    added: upsert in update space type photo

--- src/functions/graphql/schema/cache/deleteManyCacheData.ts
+++ src/functions/graphql/schema/cache/deleteManyCacheData.ts
@@ -1,4 +1,5 @@
 import { IFieldResolver } from "@graphql-tools/utils";
+import { environment } from "@utils/environment";
 import { gql } from "apollo-server-core";
 import { Context } from "../../context";
 
@@ -9,7 +10,7 @@ type DeleteManyCacheDataResult = Promise<String>;
 type DeleteManyCacheData = IFieldResolver<any, Context, DeleteManyCacheDataArgs, DeleteManyCacheDataResult>;
 
 const deleteManyCacheData: DeleteManyCacheData = async (_, { key }, { dataSources }) => {
-    const cacheKey = key.replace("cache-data-source:", "");
+    const cacheKey = key.replace(`${environment.APP_NAME}:`, "");
     const data = await dataSources.redis.deleteMany(cacheKey);
     return JSON.stringify(data);
 };
