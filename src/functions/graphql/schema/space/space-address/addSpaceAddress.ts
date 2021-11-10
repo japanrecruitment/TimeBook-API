95d932f7811e7253d8cc6f8a5ac07a9e090dad34
susantlalshrestha <susantlalshrestha@gmail.com>
2021-11-10 19:18:57 +0545

    added: code to update geoloc in space algolia index

--- src/functions/graphql/schema/space/space-address/addSpaceAddress.ts
+++ src/functions/graphql/schema/space/space-address/addSpaceAddress.ts
@@ -60,6 +60,7 @@ const addSpaceAddress: AddSpaceAddress = async (_, { spaceId, address }, { authD
     await dataSources.spaceAlgolia.partialUpdateObject({
         objectID: newAddress.spaceId,
         prefecture: newAddress.prefecture.name,
+        _geoloc: latitude && longitude ? { lat: latitude, lng: longitude } : undefined,
     });
 
     return { address: newAddress, result: { message: `Successfully added address in your space` } };
