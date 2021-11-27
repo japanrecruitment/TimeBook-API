c6f6481b3fdb1fd24a3142e8f6ad4042ae581685
susantlalshrestha <susantlalshrestha@gmail.com>
2021-11-27 20:19:24 +0545

    added: query to fetch users reservations

--- src/functions/graphql/schema/reservation/index.ts
+++ src/functions/graphql/schema/reservation/index.ts
@@ -1,8 +1,14 @@
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+import { myReservationsResolvers, myReservationsTypeDefs } from "./myReservations";
 import { reservationObjectTypeDefs } from "./ReservationObject";
 import { reservationStatusResolver, reservationStatusTypeDef } from "./ReservationStatus";
 import { reserveSpaceResolvers, reserveSpaceTypeDefs } from "./reserveSpace";
 
-export const typeDefs = mergeTypeDefs([reservationStatusTypeDef, reservationObjectTypeDefs, reserveSpaceTypeDefs]);
+export const typeDefs = mergeTypeDefs([
+    reservationStatusTypeDef,
+    reservationObjectTypeDefs,
+    reserveSpaceTypeDefs,
+    myReservationsTypeDefs,
+]);
 
-export const resolvers = mergeResolvers([reservationStatusResolver, reserveSpaceResolvers]);
+export const resolvers = mergeResolvers([reservationStatusResolver, reserveSpaceResolvers, myReservationsResolvers]);
---
+++ src/functions/graphql/schema/reservation/myReservations.ts
@@ -0,0 +1,62 @@
+import { ReservationStatus, Prisma } from "@prisma/client";
+import { IFieldResolver } from "@graphql-tools/utils";
+import { Log } from "@utils/logger";
+import { gql } from "apollo-server-core";
+import { mapSelections } from "graphql-map-selections";
+import { Context } from "../../context";
+import {
+    createPaginationResult,
+    createPaginationResultType,
+    PaginationOption,
+    PaginationResult,
+} from "../core/pagination";
+import { ReservationObject, toReservationSelect } from "./ReservationObject";
+
+type MyReservationFilter = {
+    sortOrder: Prisma.SortOrder;
+    status: ReservationStatus;
+};
+
+type MyReservationsArgs = { paginate: PaginationOption; filter: MyReservationFilter };
+
+type MyReservationsResult = Promise<PaginationResult<ReservationObject>>;
+
+type MyReservations = IFieldResolver<any, Context, MyReservationsArgs, MyReservationsResult>;
+
+const myReservations: MyReservations = async (_, { paginate, filter }, { authData, store }, info) => {
+    const { accountId } = authData;
+
+    const { take, skip } = paginate || {};
+
+    const { sortOrder, status } = filter || {};
+
+    const myReservations = await store.reservation.findMany({
+        where: { reserveeId: accountId, status },
+        ...toReservationSelect(mapSelections(info).data),
+        orderBy: { updatedAt: sortOrder },
+        take: take && take + 1,
+        skip,
+    });
+
+    Log(myReservations);
+
+    return createPaginationResult(myReservations, take, skip);
+};
+
+export const myReservationsTypeDefs = gql`
+    ${createPaginationResultType("MyReservationsResult", "ReservationObject")}
+
+    input MyReservationFilter {
+        sortOrder: SortOrder
+        status: ReservationStatus
+    }
+
+    type Query {
+        myReservations(paginate: PaginationOption, filter: MyReservationFilter): MyReservationsResult
+            @auth(requires: [user])
+    }
+`;
+
+export const myReservationsResolvers = {
+    Query: { myReservations },
+};
