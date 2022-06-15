af86061929bd2c168be63a6c111a93201b34efae
vnshgrg <ghale.avinash@gmail.com>
2022-06-16 06:17:10 +0900

    changed serverless package individually to false

--- serverless.ts
+++ serverless.ts
@@ -7,7 +7,7 @@ const serverlessConfiguration: AWS = {
     service: "pocketseq-api",
     frameworkVersion: "3",
     useDotenv: true,
-    package: { individually: true },
+    package: { individually: false },
     provider: {
         name: "aws",
         runtime: "nodejs14.x",
