2acb5165c1836681fb3846af2e2e579394ef777e
vnshgrg <ghale.avinash@gmail.com>
2022-06-27 04:04:57 +0900

    removed schema from photo Object ;

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -2,6 +2,7 @@ export { default as graphql } from "./graphql";
 export { default as emailWorker } from "./email-worker";
 export { default as webhook } from "./webhook";
 export { default as photoObject } from "./photo-object";
+
 // export { default as mediaResizer } from "./media-resizer";
 // export { default as reauthTransactions } from "./reauth-transactions";
 // export { default as transactionWorker } from "./transaction-worker";
